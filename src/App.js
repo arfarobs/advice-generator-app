1d826ca42540ed4378214275f8e62b0de2714d43
arfarobs <arthurroberts@mail.com>
2022-03-25 13:00:24 +0800

    Bulk completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import dividerPatternMobile from './images/pattern-divider-mobile.svg';
+import dividerPatternDesktop from './images/pattern-divider-desktop.svg';
+import dice from './images/icon-dice.svg';
+import { useState } from 'react';
 
 function App() {
+  const [id, setId] = useState('117');
+  const [advice, setAdvice] = useState("It is easy to sit up and take notice, what's difficult is getting up and taking action.")
+
+  const getAdvice = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        setId(jsonResponse.slip.id);
+        setAdvice(jsonResponse.slip.advice);
+      }
+    } catch(error) {
+      console.log(error);
+      setId('N/A');
+      setAdvice("I'm on strike leave me alone! (The request for new advice failed, please tell Arthur.)");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article>
+        <h1>Advice #{id}</h1>
+        <q>{advice}</q>
+        <div className='small-mobile-line'></div>
+        <picture>
+          <source media='(min-width: 580px)' srcSet={dividerPatternDesktop}/>
+          <source media='(min-width: 0px)' srcSet={dividerPatternMobile}/>
+          <img src={dividerPatternMobile} alt='' aria-hidden='true'/>
+        </picture>
+        <button onClick={getAdvice}><img src={dice} alt='A dice buton. Press to reveal new advice.'/></button>
+    </article>
   );
 }
 
